8e238ce1f80c5d70c46057777ada2af455e41219
Timo Mämecke <timo@maemecke.com>
2022-09-06 23:40:06 +0200

    Try chrome-aws-lambda

--- src/lib/screenshot.ts
+++ src/lib/screenshot.ts
@@ -1,7 +1,11 @@
-import playwright from 'playwright'
+import { chromium } from 'playwright-core'
+import lambdaChromium from 'chrome-aws-lambda'
 
 export async function screenshot(url: string) {
-  const browser = await playwright.chromium.launch()
+  const browser = await chromium.launch({
+    executablePath: await lambdaChromium.executablePath,
+    args: lambdaChromium.args,
+  })
   const context = await browser.newContext()
   const page = await context.newPage()
   await page.setViewportSize({ width: 1200, height: 630 })
