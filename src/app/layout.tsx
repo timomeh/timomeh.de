4c190c4d6329707cf91423a2698c5c13ab7592c6
Timo Mämecke <timo@maemecke.com>
2022-12-12 21:10:56 +0100

    Make navigation hover work again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,18 @@
-import clsx from 'clsx'
 import Link from 'next/link'
-import { GithubLogo } from '../components/GithubLogo'
 import { Logo } from '../components/Logo'
-import { MastodonLogo } from '../components/MastodonLogo'
 
 import '@fontsource/inter/latin.css'
 import '@fontsource/outfit/latin.css'
 import '@fontsource/ibm-plex-mono/500.css'
 import 'prism-themes/themes/prism-one-light.css'
 import '../styles/globals.css'
+import { Navigation } from './Navigation'
 
 type Props = {
   children: React.ReactNode
-  params: {
-    slug?: string
-  }
 }
 
-export default function RootLayout({ children, params }: Props) {
+export default function RootLayout({ children }: Props) {
   return (
     <html className="antialiased [font-feature-settings:'ss01']" lang="en">
       <body>
@@ -33,51 +28,8 @@ export default function RootLayout({ children, params }: Props) {
                 <Link href="/" className="block p-2 -m-2">
                   <Logo className="h-8 w-auto fill-black/80" />
                 </Link>
-
                 <div className="flex-1" />
-
-                <div className="text-sm flex font-medium text-black/60 space-x-6 items-center">
-                  <Link
-                    href="/"
-                    className="text-black/80 hover:text-black/90 transition-colors"
-                  >
-                    Hello
-                  </Link>
-
-                  <Link
-                    href="/posts"
-                    className="text-black/80 hover:text-black/90 transition-colors"
-                  >
-                    Posts
-                  </Link>
-
-                  {/* <a
-                    href="https://twitter.com/timomeh"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className=" fill-black/60 hover:fill-black/90 transition-colors"
-                  >
-                    <TwitterLogo className="w-6 h-6 -m-1 p-1" />
-                  </a> */}
-
-                  <a
-                    href="https://mastodon.social/@timomeh"
-                    rel="noopener noreferrer me"
-                    target="_blank"
-                    className=" fill-black/60 hover:fill-black/90 transition-colors"
-                  >
-                    <MastodonLogo className="w-6 h-6 -m-1 p-1" />
-                  </a>
-
-                  <a
-                    href="https://github.com/timomeh/timomeh.de"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className=" fill-black/60 hover:fill-black/90 transition-colors"
-                  >
-                    <GithubLogo className="w-[26px] h-[26px] -m-1 p-1" />
-                  </a>
-                </div>
+                <Navigation />
               </nav>
             </div>
           </div>
