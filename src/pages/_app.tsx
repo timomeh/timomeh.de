f4298699ec77ae48ec9f361ea25159b5566bbc04
Timo Mämecke <timo@maemecke.com>
2022-12-12 22:42:23 +0100

    Bring back old errors

---
+++ src/pages/_app.tsx
@@ -0,0 +1,39 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import '@fontsource/inter/latin.css'
+import '@fontsource/outfit/latin.css'
+import '@fontsource/ibm-plex-mono/500.css'
+import 'prism-themes/themes/prism-one-light.css'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title key="title">Timo Mämecke</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
