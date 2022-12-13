cc53876462c5278907b0fab1897bc02f0fa4ca79
Timo Mämecke <timo@maemecke.com>
2022-12-13 17:05:07 +0100

    Loadingrocket

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,12 @@ module.exports = {
         'gradient-radial':
           'radial-gradient(ellipse at top, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        flame: {
+          '0%, 100%': { transform: 'translate(0.3px,-0.3px)' },
+          '50%': { transform: 'translate(-0.8px,0.8px)' },
+        },
+      },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
         display: ['Outfit', ...defaultTheme.fontFamily.sans],
