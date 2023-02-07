58e9b73d16790c147f9277f15367c4c10dd268b7
Timo Mämecke <timo@maemecke.com>
2023-02-07 01:52:32 +0100

    Okay I guess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { Prose } from '@/components/Prose'
 import { redirect } from 'next/navigation'
 
 export default async function Home() {
   redirect('/offtopic')
+
+  return (
+    <main className="meh-main">
+      <div className="mx-4">
+        <Prose>
+          <p>Redirecting...</p>
+        </Prose>
+      </div>
+    </main>
+  )
 }
