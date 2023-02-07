6f4cbe6d35daae338e97ea68903e8dc4cf3f00e8
Timo Mämecke <timo@maemecke.com>
2023-02-07 22:36:40 +0100

    Maybe, who the f knows at this point

--- next.config.js
+++ next.config.js
@@ -21,31 +21,29 @@ const nextConfig = {
     ],
   },
   async rewrites() {
-    return {
-      afterFiles: [
-        {
-          source: '/assets/og-image/:category(posts|offtopic)/:slug.png',
-          destination:
-            '/_next/image?url=https://timomeh.de/api/og?slug=:slug&category=:category&w=1200&q=100',
-        },
-        {
-          source: '/:type(posts|offtopic)/feed.:format(rss|atom|json)',
-          destination: '/api/feed?format=:format&type=:type',
-        },
-        {
-          source: '/posts{/}?',
-          destination: '/posts/page/1',
-        },
-        {
-          source: '/offtopic{/}?',
-          destination: '/offtopic/page/1',
-        },
-        {
-          source: '/{/}?',
-          destination: '/offtopic/page/1',
-        },
-      ],
-    }
+    return [
+      {
+        source: '/assets/og-image/:category(posts|offtopic)/:slug.png',
+        destination:
+          '/_next/image?url=https://timomeh.de/api/og?slug=:slug&category=:category&w=1200&q=100',
+      },
+      {
+        source: '/:type(posts|offtopic)/feed.:format(rss|atom|json)',
+        destination: '/api/feed?format=:format&type=:type',
+      },
+      {
+        source: '/posts{/}?',
+        destination: '/posts/page/1',
+      },
+      {
+        source: '/offtopic{/}?',
+        destination: '/offtopic/page/1',
+      },
+      {
+        source: '/{/}?',
+        destination: '/offtopic/page/1',
+      },
+    ]
   },
   async redirects() {
     return [
