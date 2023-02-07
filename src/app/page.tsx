9d974b0396fe37adf59f6636985161f59f36ce43
Timo Mämecke <timo@maemecke.com>
2023-02-07 01:41:47 +0100

    What do I know

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Prose } from '@/components/Prose'
+
+export default async function Home() {
+  return (
+    <main className="meh-main">
+      <div className="mx-4">
+        <Prose>
+          <p>Redirecting...</p>
+        </Prose>
+      </div>
+    </main>
+  )
+}
