badc0e7ed32232cf7c0f987f21c9e1bbe5710594
Timo Mämecke <timo@maemecke.com>
2023-02-11 19:49:57 +0100

    BeforeFiles rewrites

--- next.config.js
+++ next.config.js
@@ -22,30 +22,32 @@ const nextConfig = {
     ],
   },
   async rewrites() {
-    return [
-      {
-        source: '/assets/og-image/:category(posts|offtopic)/:slug.png',
-        destination:
-          '/_next/image?url=https://timomeh.de/api/og?slug=:slug&category=:category&w=1200&q=100',
-      },
-      {
-        source: '/:type(posts|offtopic)/feed.:format(rss|atom|json)',
-        destination: '/api/feed?format=:format&type=:type',
-      },
-      // This SHOULD work but: https://github.com/vercel/next.js/issues/40549
-      // {
-      //   source: '/posts',
-      //   destination: '/posts/page/1',
-      // },
-      // {
-      //   source: '/offtopic',
-      //   destination: '/offtopic/page/1',
-      // },
-      // {
-      //   source: '/',
-      //   destination: '/offtopic/page/1',
-      // },
-    ]
+    return {
+      beforeFiles: [
+        {
+          source: '/assets/og-image/:category(posts|offtopic)/:slug.png',
+          destination:
+            '/_next/image?url=https://timomeh.de/api/og?slug=:slug&category=:category&w=1200&q=100',
+        },
+        {
+          source: '/:type(posts|offtopic)/feed.:format(rss|atom|json)',
+          destination: '/api/feed?format=:format&type=:type',
+        },
+      ],
+    }
+    // This SHOULD work but: https://github.com/vercel/next.js/issues/40549
+    // {
+    //   source: '/posts',
+    //   destination: '/posts/page/1',
+    // },
+    // {
+    //   source: '/offtopic',
+    //   destination: '/offtopic/page/1',
+    // },
+    // {
+    //   source: '/',
+    //   destination: '/offtopic/page/1',
+    // },
   },
   async redirects() {
     return [
