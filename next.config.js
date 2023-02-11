d485d89634701a72d15d55a77ca12d0ba6b737dc
Timo Mämecke <timo@maemecke.com>
2023-02-11 19:59:46 +0100

    Fix broken rewrite

--- next.config.js
+++ next.config.js
@@ -25,9 +25,9 @@ const nextConfig = {
     return {
       beforeFiles: [
         {
-          source: '/assets/og-image/:category(posts|offtopic)/:slug.png',
+          source: '/assets/og-image/:category/:slug.png',
           destination:
-            '/_next/image?url=https://timomeh.de/api/og?slug=:slug&category=:category&w=1200&q=100',
+            '/_next/image?url=https://timomeh.de/api/og%3Fslug=:slug%26category=:category&w=1200&q=100',
         },
         {
           source: '/:type(posts|offtopic)/feed.:format(rss|atom|json)',
