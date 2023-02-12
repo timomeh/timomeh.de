361bdc96a363bf6229952808c8fab13c7327320c
Timo Mämecke <timo@maemecke.com>
2023-02-12 22:11:56 +0100

    Fix cover and blur misaligned

--- src/components/FulltopCover.tsx
+++ src/components/FulltopCover.tsx
@@ -3,30 +3,25 @@ import Image from 'next/image'
 
 type Props = {
   image: string
-  title: string
-  postedAt: Date
 }
 
-export async function FulltopCover({ image, title }: Props) {
-  const size = await probeImageSize(image)
-
+export async function FulltopCover({ image }: Props) {
   return (
     <>
       <div
-        className="meh-fulltop z-[-1] relative -mb-32 lg:-mb-36 select-none mix-blend-lighten"
+        className="meh-fulltop z-[-1] relative mb-[-100px] lg:mb-[-120px] h-[240px] lg:h-[380px] select-none mix-blend-lighten overflow-hidden"
         aria-hidden
       >
-        <div className="relative opacity-75">
+        <div className="relative opacity-75 h-full w-full">
           <Image
             src={image}
-            width={size.width}
-            height={size.height}
-            className="h-64 lg:h-96 w-full object-cover"
+            className="absolute inset-0 object-cover"
+            fill
             alt=""
           />
         </div>
       </div>
-      <div className="meh-fulltop-cover h-36 lg:h-[148px] -mt-4">
+      <div className="meh-fulltop-cover h-[100px] lg:h-[120px] -mt-5">
         <div className="bg-[#151416]/60 backdrop-blur-xl rounded-t-lg w-auto h-full max-w-content mx-auto lg:mx-0" />
       </div>
     </>
