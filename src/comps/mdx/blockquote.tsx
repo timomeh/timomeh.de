9d3a26aa7f9bd1d5ac4541dd99749e7a9f884a75
Timo Mämecke <timo@maemecke.com>
2024-03-01 00:04:04 +0100

    Pixel blockquote

--- src/comps/mdx/blockquote.tsx
+++ src/comps/mdx/blockquote.tsx
@@ -11,19 +11,17 @@ export function Blockquote({ children, ...rest }: Props) {
     >
       <div className="absolute -inset-y-4 inset-x-0 shadow-xl sm:-inset-x-4 sm:-inset-y-5">
         <div className="absolute bottom-0 left-[1px] top-0 opacity-50">
-          <div className="size-3 rounded-tl-full border-l-[2px] border-t-[2px] border-yellow-300" />
-          <div className="absolute bottom-3 top-3 w-0 border-l-[2px] border-yellow-300" />
-          <div
-            className="absolute right-full top-4 size-0 border-x-[4px] border-y-[5px]
-              border-transparent border-r-transparent border-r-yellow-300 border-t-yellow-300"
-          />
-          <div
-            className="absolute bottom-0 size-3 rounded-bl-full border-b-[2px] border-l-[2px]
-              border-yellow-300"
-          />
+          <div className="absolute bottom-0 top-0 w-0 border-l-[2px] border-yellow-300" />
+          <div className="absolute right-full top-4 -mr-px w-[15px]">
+            <div className="h-[3px] w-[15px] bg-yellow-300" />
+            <div className="ml-[3px] h-[3px] w-[12px] bg-yellow-300" />
+            <div className="ml-[6px] h-[3px] w-[9px] bg-yellow-300" />
+            <div className="ml-[9px] h-[3px] w-[6px] bg-yellow-300" />
+            <div className="ml-[12px] h-[3px] w-[3px] bg-yellow-300" />
+          </div>
         </div>
-        <div className="absolute inset-y-0 left-[1px] right-0 rounded-xl bg-yellow-50/5" />
-        <div className="absolute inset-y-0 left-[1px] right-0 rounded-xl bg-crt-lines opacity-5" />
+        <div className="absolute inset-y-0 left-[1px] right-0 rounded-r-xl bg-yellow-50/5" />
+        <div className="absolute inset-y-0 left-[1px] right-0 rounded-r-xl bg-crt-lines opacity-5" />
       </div>
       <div className="relative">{children}</div>
     </blockquote>
