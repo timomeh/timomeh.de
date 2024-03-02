9c901e57a0aa546a0acf7a679bc7885ea4f4adfa
Timo Mämecke <timo@maemecke.com>
2024-03-02 23:11:35 +0100

    boxed header icons

--- src/app/@backdrop/page.tsx
+++ src/app/@backdrop/page.tsx
@@ -1,13 +1,22 @@
+import { SocialBox } from '@/comps/social-box'
+
 export default function Default() {
   return (
-    <div
-      className="absolute inset-0 z-0 flex items-center justify-start overflow-x-clip
-        sm:justify-center"
-    >
+    <>
+      <div className="absolute inset-x-0 top-0 z-10 h-0">
+        <div className="relative mx-auto max-w-2xl px-4">
+          <SocialBox />
+        </div>
+      </div>
       <div
-        className="pointer-events-none absolute -ml-[40%] h-[400px] w-[600px] rotate-6 transform
-          bg-gradient-radial from-purple-300 opacity-15 mix-blend-difference sm:ml-0"
-      />
-    </div>
+        className="pointer-events-none absolute inset-0 z-0 flex items-center justify-start
+          overflow-x-clip sm:justify-center"
+      >
+        <div
+          className="absolute -ml-[40%] h-[400px] w-[600px] rotate-6 transform bg-gradient-radial
+            from-purple-300 opacity-15 mix-blend-difference sm:ml-0"
+        />
+      </div>
+    </>
   )
 }
--- src/app/@backdrop/posts/[slug]/page.tsx
+++ src/app/@backdrop/posts/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 
+import { SocialBox } from '@/comps/social-box'
 import { getPost } from '@/lib/blog'
 
 import DefaultPage from '../../page'
@@ -20,6 +21,11 @@ export default async function Page({ params }: Props) {
 
   return (
     <Transition>
+      <div className="absolute inset-x-0 top-0 z-10 h-0">
+        <div className="relative mx-auto max-w-2xl px-4">
+          <SocialBox floating />
+        </div>
+      </div>
       <PostImage src={post.meta.cover_image} />
       <div className="absolute bottom-0 z-0 hidden w-full border-0 border-b border-white/20 sm:block" />
       <div className="relative top-[176px] z-10 mx-auto h-[83px] max-w-2xl px-4">
