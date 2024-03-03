9c64fbbc8a9e067f60f85f1ea192ec8813325a0f
Timo Mämecke <timo@maemecke.com>
2024-03-03 03:03:31 +0100

    Remove headers for better cacheability

--- src/comps/back-tag.tsx
+++ src/comps/back-tag.tsx
@@ -1,24 +1,11 @@
-import { headers } from 'next/headers'
 import Link from 'next/link'
 
 import { Tag } from './tag'
 
 export function BackTag() {
-  const referer = headers().get('referer')
-  const host = headers().get('host')?.replace(':3000', '')
-  let backHref = '/'
-
-  // if the referer was a tags page, go back there
-  if (referer) {
-    const url = new URL(referer)
-    if (url.hostname === host && url.pathname.startsWith('/tag')) {
-      backHref = url.pathname
-    }
-  }
-
   return (
     <Link
-      href={backHref}
+      href="/"
       className="ease-linear [transition:opacity_300ms,transform_60ms] hover:!opacity-100
         motion-safe:active:scale-[.97]"
     >
