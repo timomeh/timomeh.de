2dbf4a4c9a4b72a2f61949f14e5db872eaae4053
Timo Mämecke <timo@maemecke.com>
2024-11-17 02:31:06 +0100

    Fix mixed up health db and health up

--- src/app/-ah/db/route.ts
+++ src/app/-ah/db/route.ts
@@ -1,3 +1,8 @@
+import { db } from '@/data/db'
+
 export async function GET() {
+  await db.connect()
+  await db.redis.ping()
+
   return new Response('OK')
 }
--- src/app/-ah/up/route.ts
+++ src/app/-ah/up/route.ts
@@ -1,8 +1,3 @@
-import { db } from '@/data/db'
-
 export async function GET() {
-  await db.connect()
-  await db.redis.ping()
-
   return new Response('OK')
 }
