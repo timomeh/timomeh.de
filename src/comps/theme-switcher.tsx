6e4492b5da812b320e05505900407eb8ef4caf9d
Timo Mämecke <timo@maemecke.com>
2025-01-11 02:01:55 +0100

    Improve theme switcher animation

--- src/comps/theme-switcher.tsx
+++ src/comps/theme-switcher.tsx
@@ -4,8 +4,8 @@ export function ThemeSwitcher() {
   return (
     <button
       title="Switch color mode"
-      className="relative -m-1 block p-1 opacity-70 transition-opacity hover:opacity-100
-        active:scale-95"
+      className="group/btn relative -m-1 block p-1 opacity-70 transition-opacity
+        hover:opacity-100 active:scale-95"
       value="light"
       type="button"
       onClick={() => {
@@ -31,111 +31,81 @@ export function ThemeSwitcher() {
     >
       <div className="size-4">
         <svg
-          width="72"
-          height="48"
-          viewBox="0 0 72 48"
+          viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="pointer-events-none relative -left-[100%] h-[200%] w-[300%] overflow-visible
-            [&>*]:transition [&>*]:duration-1000
-            group-data-[theme=system]/root:[&>*]:duration-300"
+          className="overflow-visible [&_*]:transition-[transform,opacity] [&_*]:duration-300"
         >
-          <g
-            id="moon"
-            className="-translate-x-[50%] translate-y-[50%] opacity-0
-              group-data-[theme=dark]/root:translate-x-0
-              group-data-[theme=dark]/root:translate-y-0
-              group-data-[theme=dark]/root:opacity-100
-              dark:group-data-[theme=system]/root:translate-x-0
-              dark:group-data-[theme=system]/root:translate-y-0"
-          >
-            <path
-              id="moon-path"
-              d="M30 2H38V4H36V6H34V12H36V14H42V12H44V10H46V18H44V20H42V22H32V20H30V18H28V16H26V6H28V4H30V2Z"
-              fill="currentColor"
-            />
-          </g>
-          <g
-            id="sun"
-            className="translate-x-[50%] translate-y-[50%] opacity-0
-              group-data-[theme=light]/root:translate-x-0
-              group-data-[theme=light]/root:translate-y-0
-              group-data-[theme=system]/root:translate-x-0
-              group-data-[theme=system]/root:translate-y-0
-              group-data-[theme=light]/root:opacity-100
-              dark:group-data-[theme=system]/root:!translate-x-[50%]
-              dark:group-data-[theme=system]/root:!translate-y-[50%]"
-          >
-            <g id="sun-path">
-              <path d="M37 1H35V4H37V1Z" fill="currentColor" />
-              <path d="M37 20H35V23H37V20Z" fill="currentColor" />
+          <g className="origin-center scale-[0.6] dark:scale-100">
+            <mask
+              id="moon-mask"
+              maskUnits="userSpaceOnUse"
+              x="2"
+              y="2"
+              width="20"
+              height="20"
+            >
+              <rect x="2" y="2" width="20" height="20" fill="white" />
               <path
-                d="M32 6H40V8H42V16H40V18H32V16H30V8H32V6Z"
+                d="M2 16H4V18H6V20H8V22H2V16Z"
+                fill="black"
+                id="rotate"
+                className="translate-y-1 dark:translate-y-0"
+              />
+              <path
+                d="M14 2H22V10H20V12H18V14H12V12H10V6H12V4H14V2Z"
+                fill="black"
+                className="origin-top-right -translate-y-1 translate-x-3 dark:translate-x-0
+                  dark:translate-y-0 dark:group-hover/btn:scale-90"
+                id="crescent"
+              />
+              <rect
+                id="auto"
+                x="1"
+                y="13"
+                width="8"
+                height="10"
+                fill="black"
+                className="-translate-x-3 translate-y-3 group-data-[theme=system]/root:-translate-x-0.5
+                  group-data-[theme=system]/root:translate-y-0.5
+                  dark:group-data-[theme=system]/root:translate-x-0
+                  dark:group-data-[theme=system]/root:translate-y-0"
+              />
+            </mask>
+            <g mask="url(#moon-mask)">
+              <path
+                d="M6 2V4H4V6H2V18H4V20H6V22H18V20H20V18H22V6H20V4H18V2H6Z"
                 fill="currentColor"
               />
-              <path d="M25 11H28V13H25V11Z" fill="currentColor" />
-              <path d="M47 11H44V13H47V11Z" fill="currentColor" />
-              <path d="M28 4H30V6H28V4Z" fill="currentColor" />
-              <path d="M44 18H42V20H44V18Z" fill="currentColor" />
-              <path d="M28 18H30V20H28V18Z" fill="currentColor" />
-              <path d="M44 4H42V6H44V4Z" fill="currentColor" />
             </g>
           </g>
           <g
-            id="sun-auto"
-            className="translate-x-[50%] translate-y-[50%] opacity-0
-              group-data-[theme=light]/root:translate-x-0
-              group-data-[theme=light]/root:translate-y-0
-              group-data-[theme=system]/root:translate-x-0
-              group-data-[theme=system]/root:translate-y-0
-              group-data-[theme=system]/root:opacity-100
-              dark:group-data-[theme=system]/root:!translate-x-[50%]
-              dark:group-data-[theme=system]/root:!translate-y-[50%]
-              dark:group-data-[theme=system]/root:!opacity-0"
+            id="rays"
+            className="origin-center rotate-0 opacity-100 group-hover/btn:rotate-12 dark:rotate-45
+              dark:opacity-0"
           >
+            <path d="M13 1H11V4H13V1Z" fill="currentColor" />
+            <path d="M13 20H11V23H13V20Z" fill="currentColor" />
+            <path d="M1 11H4V13H1V11Z" fill="currentColor" />
+            <path d="M23 11H20V13H23V11Z" fill="currentColor" />
+            <path d="M4 4H6V6H4V4Z" fill="currentColor" />
+            <path d="M20 18H18V20H20V18Z" fill="currentColor" />
             <path
-              id="sun-auto-path"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M25 15V23H27V21H29V23H31V15H25ZM29 19V17H27V19H29Z"
+              d="M4 18H6V20H4V18Z"
               fill="currentColor"
+              className="group-data-[theme=system]/root:opacity-0"
             />
-            <g id="sun-half-path">
-              <path d="M37 1H35V4H37V1Z" fill="currentColor" />
-              <path d="M37 20H35V23H37V20Z" fill="currentColor" />
-              <path
-                d="M32 6H40V8H42V16H40V18H33V13H30V8H32V6Z"
-                fill="currentColor"
-              />
-              <path d="M25 11H28V13H25V11Z" fill="currentColor" />
-              <path d="M47 11H44V13H47V11Z" fill="currentColor" />
-              <path d="M28 4H30V6H28V4Z" fill="currentColor" />
-              <path d="M44 18H42V20H44V18Z" fill="currentColor" />
-              <path d="M44 4H42V6H44V4Z" fill="currentColor" />
-            </g>
+            <path d="M20 4H18V6H20V4Z" fill="currentColor" />
           </g>
-          <g
-            id="moon-auto"
-            className="-translate-x-[50%] translate-y-[50%] opacity-0
-              group-data-[theme=dark]/root:translate-x-0
-              group-data-[theme=dark]/root:translate-y-0
-              dark:group-data-[theme=system]/root:translate-x-0
-              dark:group-data-[theme=system]/root:translate-y-0
-              dark:group-data-[theme=system]/root:opacity-100"
-          >
-            <path
-              id="moon-half-path"
-              d="M30 2H38V4H36V6H34V12H36V14H42V12H44V10H46V18H44V20H42V22H33V13H26V6H28V4H30V2Z"
-              fill="currentColor"
-            />
-            <path
-              id="moon-auto-path"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M25 15V23H27V21H29V23H31V15H25ZM29 19V17H27V19H29Z"
-              fill="currentColor"
-            />
-          </g>
+          <path
+            id="a-letter"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M1 15V23H3V21H5V23H7V15H1ZM5 19V17H3V19H5Z"
+            fill="currentColor"
+            className="origin-bottom-left scale-75 opacity-0 group-data-[theme=system]/root:scale-100
+              group-data-[theme=system]/root:opacity-100"
+          />
         </svg>
       </div>
     </button>
