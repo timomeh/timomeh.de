fb8c5adb8e027adb5a22b90e2d448c1fa46f324c
Timo Mämecke <timo@maemecke.com>
2025-03-08 17:36:06 +0100

    Add sentry node profiling

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,10 @@
 import * as Sentry from '@sentry/nextjs'
+import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1,
+  profilesSampleRate: 1,
   environment: process.env.SENTRY_ENVIRONMENT,
 })
